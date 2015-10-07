ef9284dcef49d5832bf49bfd2018343e91b12170
Dario Caruso <darioyzf@gmail.com>
2015-10-07 12:49:52 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -3,10 +3,21 @@ linuxWifi=require('../index.js');
 
 linuxWifi.list().then(function(list){
   verb(list)
+}).catch(function(err){
+  verb(err,"error","LinuxWifi")
 })
 
 linuxWifi.add("dd","ss",2).then(function(){
   linuxWifi.list().then(function(list){
     verb(list)
   })
+}).catch(function(err){
+  verb(err,"error","LinuxWifi")
+})
+linuxWifi.add("ddx","sss").then(function(){
+  linuxWifi.list().then(function(list){
+    verb(list)
+  })
+}).catch(function(err){
+  verb(err,"error","LinuxWifi")
 })

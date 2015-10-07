b31e296d8357a266cea1f733bab39e08a6933e39
Dario Caruso <darioyzf@gmail.com>
2015-10-07 12:57:48 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -7,14 +7,14 @@ linuxWifi.list().then(function(list){
   verb(err,"error","LinuxWifi")
 })
 
-linuxWifi.add("dd","ss",2).then(function(){
+linuxWifi.add("dd","sdddddddddddddds",2).then(function(){
   linuxWifi.list().then(function(list){
     verb(list)
   })
 }).catch(function(err){
   verb(err,"error","LinuxWifi")
 })
-linuxWifi.add("ddx","sss").then(function(){
+linuxWifi.add("ddx","yyyyyyyyyyyyysss").then(function(){
   linuxWifi.list().then(function(list){
     verb(list)
   })
